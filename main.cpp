dba0f93bfc45da30eeda788f9dfef270ea7a6677
Davide Larosa <davide.larosa90@gmail.com>
2024-12-29 17:57:24 +0100

    update

--- main.cpp
+++ main.cpp
@@ -19,6 +19,6 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.loadFromModule("WeatherStation", "Main");
-    appBridge.restApiRequest();
+    apirest.restApiRequest();
     return app.exec();
 }
